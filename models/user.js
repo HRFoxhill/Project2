4d16b7ab11e3bbf30123c76386f14fbb5ac0c6dd
Brendon <brendonbarringer@gmail.com>
2018-07-30 13:05:19 -0500

    worked on routes

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      
+      //define user with all db elements
+    });
+  
+    Author.associate = function(models) {
+      // Associating User with Posts
+      // When an User is deleted, also delete any associated Posts
+      Author.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
+  };
---
+++ routes/html-routes.js
@@ -0,0 +1,35 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index page (guessing this is where all the posts will show up, like facebook?)
+  app.get("/", function(req, res) {
+    res.redirect("/login");
+  });
+
+  // login page
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/login.handlebars"));
+  });
+
+  // signup page
+  app.get("/signup", function(req, res) {
+    
+    res.sendFile(path.join(__dirname, "../views/signup.handlebars"));
+  });
+
+  app.get("/home", function(req,res) {
+    res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+  })
+  
+};

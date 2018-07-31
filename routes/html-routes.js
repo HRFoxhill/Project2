030ea14e00909ac30b4b92912024dde99011ae95
Brendon <brendonbarringer@gmail.com>
2018-07-31 10:48:52 -0500

    added server and connection.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,19 +12,10 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index page (guessing this is where all the posts will show up, like facebook?)
-  app.get("/", function(req, res) {
-    res.redirect("/login");
-  });
 
-  // login page
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/login.handlebars"));
-  });
 
   // signup page
-  app.get("/signup", function(req, res) {
-    
+  app.get("/", function(req, res) {    
     res.sendFile(path.join(__dirname, "../views/signup.handlebars"));
   });
 

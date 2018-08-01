6aca49c69d6155054376c78b88a7035716e4c414
jon Opacich <jonopacich@gmail.com>
2018-07-31 23:25:59 -0500

    home page mostly done

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         let distanceFromTop = $(document).scrollTop();
-        if (distanceFromTop >= $('.hero').height())
-        {
+        if (distanceFromTop >= $('.hero').height()) {
             $('#navbar').addClass('sticky');
             $('.menu').addClass('sticky-menu');
         }
-        else
-        {
+        else {
             $('#navbar').removeClass('sticky');
             $('.menu').removeClass('sticky-menu');
+
         }
     });
-})
+});

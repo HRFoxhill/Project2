9a56bd587a73dc845c82d3dcbb21dc12ddf7ad90
jon Opacich <jonopacich@gmail.com>
2018-07-31 20:45:26 -0500

    home page layout

---
+++ app/public/js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        let distanceFromTop = $(document).scrollTop();
+        if (distanceFromTop >= $('.hero').height())
+        {
+            $('#navbar').addClass('sticky');
+            $('.menu').addClass('sticky-menu');
+        }
+        else
+        {
+            $('#navbar').removeClass('sticky');
+            $('.menu').removeClass('sticky-menu');
+        }
+    });
+})

f14b1c2cada4e615ba8c6576316b5709b4be31eb
Brendon <brendonbarringer@gmail.com>
2018-08-02 19:23:40 -0500

    routes somewhat work

--- app/routes/user-api-routes.js
+++ app/routes/user-api-routes.js
@@ -2,33 +2,30 @@ var db = require("../models");
 
 module.exports = function(app) {
 app.get("/api/user", function(req, res) {
-    // db.User.findAll({
-    //   include: [db.Task]
-    // }).then(function(dbUser) {
-    //   res.json(dbUser);
-    // });
-    res.json([
-      {id:1, project_name: 'carpentry', project_description: 'Lorem Ipsum', project_hours: 5, project_status: 'open' }
-    ])
+    db.User.findAll({
+      include: [db.Project]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+    
   });
 
   app.get("/api/user/:id", function(req, res) {
-    // db.User.findOne({
-    //   where: {
-    //     id: req.params.id
-    //   },
-    //   include: [db.Task]
-    // }).then(function(dbUser) {
-    //   res.json(dbUser);
-    // });
-    res.json([
-      {id:1, project_name: 'carpentry', project_description: 'Lorem Ipsum', project_hours: 5, project_status: 'open' }
-    ])
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Project]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+   
   });
 
   app.post("/api/user", function(req, res) {
-    // db.User.create(req.body).then(function(dbUser) {}
-      res.json(req.body);
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
+    });
     });
   
 
